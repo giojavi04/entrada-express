079658d69dabafb35f5766fd0d78057789cd292b
Javier Montalvo <giojavi04@gmail.com>
2024-03-08 01:33:43 -0500

    impl: initial proyect

---
+++ src/config/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import 'firebase/auth';
+import 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCnFwR9lbaz6CCgWb2AMxiGkpgtsYIAjoY",
+  authDomain: "entrada-express.firebaseapp.com",
+  projectId: "entrada-express",
+  storageBucket: "entrada-express.appspot.com",
+  messagingSenderId: "821092776603",
+  appId: "1:821092776603:web:47521570c837a2834a643e",
+  measurementId: "G-8CYSRW1DYQ"
+};
+
+export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+export const analytics = getAnalytics(app);
