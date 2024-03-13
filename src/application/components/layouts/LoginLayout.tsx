fbfbeb0dcff678c2e25ad91be9ca3dd3c8dc19bf
Javier Montalvo <giojavi04@gmail.com>
2024-03-13 08:02:57 -0500

    fix: improvements

--- src/application/components/layouts/LoginLayout.tsx
+++ src/application/components/layouts/LoginLayout.tsx
@@ -1,29 +1,31 @@
 import Logo from "./Logo";
 
-const LoginLayout = ({ children }: { children: React.ReactNode }) => {
+const LoginLayoutComponent = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+    <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="mx-auto w-full max-w-md">
         <div className="flex justify-center">
           <Logo className="h-32 w-auto" />
         </div>
-        <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-dianne-900">
-          Ingresa en tu cuenta
+        <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight">
+          Iniciar sesión
         </h2>
       </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+      <div className="mt-10 mx-auto w-full max-w-[480px]">
         {children}
 
-        <p className="mt-10 text-center text-sm text-dianne-900">
+        <p className="mt-10 text-center text-sm">
           Desarrollado por{' '}
-          <a href="https://javiermontalvo.dev" target="_blank" rel="noreferrer noopener" className="font-semibold leading-6 text-dianne-900 hover:text-dianne-950">
-            Javier montalvo
+          <a href="https://javiermontalvo.dev" target="_blank" rel="noreferrer noopener" className="font-semibold hover:text-gray-900">
+            Javier Montalvo
           </a>
         </p>
       </div>
     </div>
   )
 };
+
+export const LoginLayout = LoginLayoutComponent;
 
 export default LoginLayout;
